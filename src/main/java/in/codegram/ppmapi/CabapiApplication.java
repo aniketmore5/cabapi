743be21ae32ebe15c9d31bf6d2342745efc1c98b
ADMIN <ADMIN@DESKTOP-CTVND1A>
2021-09-30 15:45:46 +0530

    initial commit

---
+++ src/main/java/in/codegram/ppmapi/CabapiApplication.java
@@ -0,0 +1,15 @@
+package in.codegram.ppmapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CabapiApplication.class, args);
+	}
+
+}
+
+/*gnnalkn*/
---
+++ src/test/java/in/codegram/ppmapi/CabapiApplicationTests.java
@@ -0,0 +1,13 @@
+package in.codegram.ppmapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CabapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
